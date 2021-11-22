2c3ecee82ee8fc9c08cd74c14721cb5862d23577
Ajxblood <aaronandrews.j@gmail.com>
2021-11-22 12:50:55 -0800

    Update constants.js

--- src/constants.js
+++
@@ -1,4 +0,0 @@
-export const CHANNEL_NAME = '#ChefAj';
-export const OAUTH_TOKEN = 'oauth:gkzhd2fyjxifhbh8h5zi3sd5gw7eax';
-export const BOT_USERNAME = 'chefbotop'
-export const DISC_TOKEN = 'ODkyOTI0Mzc0NTAyNTA2NTM3.YVT-oA.abbTxOnouYwkssX1_St-b_N9liM'
